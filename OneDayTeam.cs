b89c1b7331dbac9d961b5099c10aa8af8a311e61
jeeban <jeebanjyoti193@gmail.com>
2023-08-08 11:48:00 +0530

    first commit

---
+++ OneDayTeam.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Course_end_project_1
+{
+    public class OneDayTeam : ITeam
+    {
+        public static List<Player> oneDayTeam = new List<Player>();
+
+        public OneDayTeam()
+        {
+            oneDayTeam.Capacity = 11;
+        }
+
+        public void Add(Player player)
+        {
+            if (oneDayTeam.Count >= 11)
+            {
+                Console.WriteLine("Cannot add more than 11 players to the team.");
+                return;
+            }
+
+            oneDayTeam.Add(player);
+        }
+
+        public void Remove(int playerId)
+        {
+            Player player = oneDayTeam.FirstOrDefault(p => p.PlayerId == playerId);
+            if (player != null)
+            {
+                oneDayTeam.Remove(player);
+                Console.WriteLine("Player removed successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Player not found in the team.");
+            }
+        }
+
+        public Player GetPlayerById(int playerId)
+        {
+            return oneDayTeam.FirstOrDefault(p => p.PlayerId == playerId);
+        }
+
+        public Player GetPlayerByName(string playerName)
+        {
+            return oneDayTeam.FirstOrDefault(p => p.PlayerName == playerName);
+        }
+
+        public List<Player> GetAllPlayers()
+        {
+            return oneDayTeam;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course_end_project_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OneDayTeam team = new OneDayTeam();
+            string choice;
+
+            do
+            {
+                Console.WriteLine("Enter your choice:");
+                Console.WriteLine("Enter 1: To Add Player 2: To Remove Player by Id 3: Get Player By Id 4: Get Player by Name 5: Get All Players 6: Exit");
+
+                int menuChoice = int.Parse(Console.ReadLine());
+
+
+                switch (menuChoice)
+                {
+                    case 1:
+
+                        Console.Write("Enter Player Id: ");
+                        int playerId = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Enter Player Name: ");
+                        string playerName = Console.ReadLine();
+                        Console.Write("Enter Player Age: ");
+                        int playerAge = Convert.ToInt32(Console.ReadLine());
+
+                        team.Add(new Player { PlayerId = playerId, PlayerName = playerName, PlayerAge = playerAge });
+                        Console.WriteLine("Player is added Successfully");
+                        break;
+                    case 2:
+
+                        Console.Write("Enter Player Id to remove: ");
+                        int removeId = Convert.ToInt32(Console.ReadLine());
+                        team.Remove(removeId);
+                        break;
+                    case 3:
+
+                        Console.Write("Enter Player Id : ");
+                        int getId = Convert.ToInt32(Console.ReadLine());
+                        Player getPlayerById = team.GetPlayerById(getId);
+                        if (getPlayerById != null)
+                        {
+                            Console.WriteLine($"Player {getPlayerById.PlayerName} is {getPlayerById.PlayerAge} years old.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Player not found.");
+                        }
+                        break;
+                    case 4:
+
+                        Console.Write("Enter Player Name : ");
+                        string getName = Console.ReadLine();
+                        Player getPlayerByName = team.GetPlayerByName(getName);
+                        if (getPlayerByName != null)
+                        {
+                            Console.WriteLine($"Player {getPlayerByName.PlayerName} is {getPlayerByName.PlayerAge} years old.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Player not found.");
+                        }
+                        break;
+                    case 5:
+
+                        List<Player> allPlayers = team.GetAllPlayers();
+                        foreach (var player in allPlayers)
+                        {
+                            Console.WriteLine($"Player Id: {player.PlayerId}, Name: {player.PlayerName}, Age: {player.PlayerAge}");
+                        }
+                        break;
+                    case 6:
+
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+                Console.Write("Do you want to continue (yes/no)?: ");
+                choice = Console.ReadLine();
+            }
+            while (choice.ToLower() == "yes");
+
+        }
+    }
+}
